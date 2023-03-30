8d42286d7cf28ee5eeb068a0d8620f4bfd0a336a
kajolroy <shrikajol@gmail.com>
2023-03-31 01:19:46 +0600

    fix product grid

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
         <GridSpan />
       </div>
       <div className="container mx-auto">
-        <div className="grid lg:grid-cols-6 sm:grid-cols-5  grid-cols-2 gap-2 my-10">
+        <div className="grid lg:grid-cols-6 sm:grid-cols-5 sm:grid-cols-4  grid-cols-2 gap-2 my-10">
           {data.map((item) => (
             <Product item={item} key={item.id} />
           ))}
