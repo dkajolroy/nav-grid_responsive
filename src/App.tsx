7d1be76270d62df59efb50d94d0ac81032ff1f3f
kajolroy <shrikajol@gmail.com>
2023-03-31 01:12:42 +0600

    Col span grid systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import GridSpan from "./components/GridSpan";
 import Header from "./components/Header";
 import Product from "./components/Product";
 import { data } from "./constants/dummy";
@@ -6,6 +7,9 @@ export default function App() {
   return (
     <div>
       <Header />
+      <div className="container mx-auto">
+        <GridSpan />
+      </div>
       <div className="container mx-auto">
         <div className="grid lg:grid-cols-6 sm:grid-cols-5  grid-cols-2 gap-2 my-10">
           {data.map((item) => (
--- src/components/GridSpan.tsx
+++ src/components/GridSpan.tsx
@@ -1,3 +1,12 @@
 export default function GridSpan() {
-  return <div>GridSpan</div>;
+  return (
+    <div className="grid grid-cols-5 ">
+      <div className="col-span-4 bg-red-200 h-[200px] flex items-center justify-center">
+        Banner
+      </div>
+      <div className="col-span-1 bg-yellow-200 h-[200px] flex items-center justify-center">
+        Banner mini
+      </div>
+    </div>
+  );
 }
